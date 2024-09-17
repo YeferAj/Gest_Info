497cf53942c1872f354490bf5703a7158ac6c61a
Yeff <Yeff@gmail.com>
2024-09-17 07:10:10 -0500

    Actualizacion accountRoutes

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const accountController = require('../controllers/accountController'); // Asegúrate de que esta ruta es correcta
+const accountController = require('../controllers/accountController'); 
 
 // Rutas para cuentas
-router.post('/', accountController.createAccount); // Asegúrate de que createAccount esté definido en accountController
-router.post('/consign', accountController.consignMoney); // Asegúrate de que consignMoney esté definido en accountController
-router.post('/withdraw', accountController.withdrawMoney); // Asegúrate de que withdrawMoney esté definido en accountController
-router.delete('/:numeroCuenta', accountController.deleteAccount); // Asegúrate de que deleteAccount esté definido en accountController
+router.post('/', accountController.createAccount); 
+router.post('/consign', accountController.consignMoney); 
+router.post('/withdraw', accountController.withdrawMoney);
+router.delete('/:numeroCuenta', accountController.deleteAccount); 
 
 module.exports = router;
